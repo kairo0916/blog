0de670fb02aa0ee672ef17e6fac46b5470419402
Kairo <kairo.tw0916@gmail.com>
2026-01-04 00:09:23 +0800

    Change theme color hue from 361 to 290

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "一個分享網站開發、程式實作、自動化工具與技術經驗的博客，作者Kairo",
 	lang: "zh_TW",
 	themeColor: {
-		hue: 361,
+		hue: 290,
 		fixed: false,
     forceDarkMode: false,
 	},
