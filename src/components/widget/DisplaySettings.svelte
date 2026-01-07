189e4e8383a04a3ebba3a1d5c0b40a80693e3dc9
Kairo <kairo.tw0916@gmail.com>
2026-01-08 03:16:35 +0800

    Update DisplaySettings.svelte

--- src/components/widget/DisplaySettings.svelte
+++ src/components/widget/DisplaySettings.svelte
@@ -6,7 +6,6 @@ import {
 	getHue,
 	getStoredTheme,
 	setBgHueRotate,
-	setHideBg,
 	setHue,
 	setTheme,
 } from "@utils/setting-utils";
@@ -101,37 +100,6 @@ let hideBg = getHideBg();
         <input aria-label="主題色彩" type="range" min="0" max="360" bind:value={hue} disabled={isRainbowMode}
                class="slider" id="colorSlider" step="1" style="width: 100%">
     </div>
-
-    <div class="flex flex-row gap-2 mb-3 items-center justify-between">
-        <div class="flex gap-2 font-bold text-lg text-neutral-900 dark:text-neutral-100 transition relative ml-3
-            before:w-1 before:h-4 before:rounded-md before:bg-[var(--primary)]
-            before:absolute before:-left-3 before:top-[0.33rem]"
-        >
-            禁用背景
-        </div>
-        <input type="checkbox" class="toggle-switch" checked={hideBg} on:change={toggleHideBg} />
-    </div>
-
-    <div class="flex flex-row gap-2 mb-3 mt-3 items-center justify-between">
-        <div class="flex gap-2 font-bold text-lg text-neutral-900 dark:text-neutral-100 transition relative ml-3
-            before:w-1 before:h-4 before:rounded-md before:bg-[var(--primary)]
-            before:absolute before:-left-3 before:top-[0.33rem]"
-        >
-            背景模糊
-        </div>
-        <div class="flex gap-1">
-            <div class="transition bg-[var(--btn-regular-bg)] w-10 h-7 rounded-md flex justify-center
-            font-bold text-sm items-center text-[var(--btn-content)]">
-                {bgBlur}px
-            </div>
-        </div>
-    </div>
-    <div class="w-full h-6 bg-[var(--btn-regular-bg)] rounded select-none overflow-hidden">
-        <input aria-label="背景模糊" type="range" min="0" max="20" bind:value={bgBlur}
-               class="slider" step="1" style="width: 100%; --value-percent: {bgBlur / 20 * 100}%">
-    </div>
-</div>
-
 
 <style lang="stylus">
     #display-setting
